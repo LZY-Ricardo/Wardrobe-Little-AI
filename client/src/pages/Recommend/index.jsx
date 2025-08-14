349fc37e08b360d815f4f792d3075eed97865312
Ricardo <3239468786lzy@gmail.com>
2025-08-15 00:49:20 +0800

    update

--- client/src/pages/Recommend/index.jsx
+++ client/src/pages/Recommend/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from './index.module.less'
+import SvgIcon from '@/components/SvgIcon'
 
 
 export default function Recommend() {
   return (
     <div className={styles['recommend']}>
       <div className={styles['recommend-header']}>
-        <input type="text" />
-        <button></button>
+        <SvgIcon iconName="icon-icon-sousuo" className={styles['search-icon']}/>
+        <input type="text" placeholder="请输入场景(如约会、运动)" />  
+        <button>确认</button>
       </div>
     </div>
   )
